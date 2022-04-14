e11d2271e78ea53aa8fed67b3ac59ffdcdfb9880
Bobby Dhillon <bdhillon1994@gmail.com>
2022-04-14 01:18:41 -0700

    Json from stdin, added Makefile and some end to end tests

--- Makefile
+++ Makefile
@@ -2,8 +2,15 @@ build:
 	go build -o flights
 
 test: build
+# unit tests
 	go test -v ./...
-	./flights -jsonFile testdata/input.json
+# end to end tests
+	echo '[["SFO", "ATL"]]' | ./flights || (echo "failed $$?"; exit 1)
+	echo '[["ATL", "EWR"],["SFO", "ATL"]]' | ./flights  || (echo "failed $$?"; exit 1)
+	echo '[["IND", "EWR"], ["SFO", "ATL"], ["GSO", "IND"], ["ATL", "GSO"]]' | ./flights || (echo "failed $$?"; exit 1)
+	echo '[["EWR", "IND"], ["SFO", "ATL"], ["GSO", "IND"], ["ATL", "GSO"]]' | ./flights || (echo "failed $$?"; exit 0)
+	echo '[["IND", "IND"]]' | ./flights || (echo "failed $$?"; exit 0)
+	echo '[["IND", "SFO"],["SFO", "IND"]]' | ./flights || (echo "failed $$?"; exit 0)
 
 clean:
 	rm flights
--- main.go
+++ main.go
@@ -2,49 +2,35 @@ package main
 
 import (
 	"encoding/json"
-	"flag"
 	"fmt"
-	"io"
 	"log"
 	"os"
 	"volumefi-golang-assignment/flight"
 )
 
-const defaultJsonPath = "testdata/input.json"
-
-// parseJsonFile reads in a json file and parses it.
-func parseJsonFile(jsonFilePath string) ([][]string, error) {
-	jsonFile, err := os.Open(jsonFilePath)
+// decodeJson reads in a json from stdin and decodes it.
+func decodeJson() ([][]string, error) {
+	var jsonData [][]string
+	err := json.NewDecoder(os.Stdin).Decode(&jsonData)
 	if err != nil {
 		return nil, err
 	}
-	defer jsonFile.Close()
-
-	b, _ := io.ReadAll(jsonFile)
-	var flights [][]string
-	err = json.Unmarshal(b, &flights)
-	if err != nil {
-		return nil, err
-	}
-	return flights, nil
+	return jsonData, nil
 }
 
 func main() {
-	var jsonFile string
-	flag.StringVar(&jsonFile, "jsonFile", defaultJsonPath, "input json file path")
-
-	flights, err := parseJsonFile(jsonFile)
+	flights, err := decodeJson()
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(1)
 	}
 
 	srcDest, err := flight.SortPath(flights)
-	// flights.SortPath(flights)
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(1)
 	}
 
 	fmt.Println(string(srcDest))
+	os.Exit(0)
 }
