adfff1a473a56bbf3ddfe1ffa1165167c8a1334b
Bobby Dhillon <bdhillon1994@gmail.com>
2022-04-13 21:51:45 -0700

    Add json unmarshal + error on disconnected path

--- main.go
+++ main.go
@@ -1,16 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	flightGraph := make(map[string]string)
-	flights := [][]string{
-		{"IND", "EWR"},
-		{"SFO", "ATL"},
-		{"GSO", "IND"},
-		{"ATL", "GSO"},
+	dataJson := `[["IND", "EWR"],["SFO", "ATL"],["GSO", "IND"],["ATL", "GSO"]]`
+	var flights [][]string
+	err := json.Unmarshal([]byte(dataJson), &flights)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
 	}
 
+	flightGraph := make(map[string]string)
 	destAirports := make(map[string]string)
 	for _, flight := range flights {
 		src, dest := flight[0], flight[1]
@@ -18,15 +24,21 @@ func main() {
 		destAirports[dest] = ""
 	}
 
-	// find the src, or the one with zero in bound edges
-	var src string
-	for s := range flightGraph {
-		if _, ok := destAirports[s]; !ok {
-			src = s
+	// find all the airports with zero in bound flights
+	var sources []string
+	for src := range flightGraph {
+		if _, ok := destAirports[src]; !ok {
+			sources = append(sources, src)
 		}
 	}
 
+	if len(sources) > 1 {
+		log.Fatalf("flight path is disconnected")
+		os.Exit(1)
+	}
+
 	var sortedFlights [][]string
+	src := sources[0]
 	for len(sortedFlights) != len(flights) {
 		dest := flightGraph[src]
 		sortedFlights = append(sortedFlights, []string{src, dest})
